f1aa7d07114841709a4490bbd9a9f3e15788c18a
Dandiaz14 <39220414+Dandiaz14@users.noreply.github.com>
2018-05-14 13:21:19 -0500

    Add files via upload

---
+++ academicformation.cpp
@@ -0,0 +1,78 @@
+#include "academicformation.h"
+
+Formation::Formation()
+{
+    gradeName = "NON";
+    institution = "NON";
+    idCard = "NON";
+}
+
+///Formation::Formation(const Formation&){} ///CONSTRUCTOR COPIA
+
+///Constructor Parametrizado
+Formation::Formation(const std::string& n,std::string& i,Date& b,Date& f,Date& d,std::string& id) : gradeName(n),institution(i),beginDate(b),finishDate(f),degreeDate(d),idCard(id) {}
+
+std::string Formation::getGradeName()
+{
+    return gradeName;
+}
+
+std::string Formation::getInstitution()
+{
+    return institution;
+}
+
+Date Formation::getBeginDate()
+{
+    return beginDate;
+}
+
+Date Formation::getFinishDate()
+{
+    return finishDate;
+}
+
+Date Formation::getDegreeDate()
+{
+    return degreeDate;
+}
+
+std::string Formation::getIdCard()
+{
+    return idCard;
+}
+
+void Formation::setGradeName(const std::string& n)
+{
+    gradeName = n;
+}
+
+void Formation::setInstitution(const std::string& i)
+{
+    institution = i;
+}
+
+void Formation::setBeginDate(const Date& b)
+{
+    beginDate = b;
+}
+
+void Formation::setFinishDate(const Date& f)
+{
+    finishDate = f;
+}
+
+void Formation::setDegreeDate(const Date& d)
+{
+    degreeDate = d;
+}
+
+void Formation::setIdCard(const std::string& id)
+{
+    idCard = id;
+}
+
+/*void Formation::toString()
+{
+
+}*/
