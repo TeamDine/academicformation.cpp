61804c95917e77f61c5c367e88841c24db42c795
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-20 09:58:45 -0500

    Actualización de la clase
    Se cambia el atributo Nombre (de tipo Name) a (tipo string)

--- academicformation.cpp
+++ academicformation.cpp
@@ -1,78 +1,73 @@
 #include "academicformation.h"
 
-Formation::Formation()
-{
+Formation::Formation() {
+    grade = "NON";
     gradeName = "NON";
     institution = "NON";
     idCard = "NON";
-}
+    }
 
 ///Formation::Formation(const Formation&){} ///CONSTRUCTOR COPIA
 
 ///Constructor Parametrizado
-Formation::Formation(const std::string& n,std::string& i,Date& b,Date& f,Date& d,std::string& id) : gradeName(n),institution(i),beginDate(b),finishDate(f),degreeDate(d),idCard(id) {}
+Formation::Formation(const std::string& n,std::string& i, std::string& g, Date& b,Date& f,Date& d,std::string& id) : grade(g), gradeName(n),institution(i),beginDate(b),finishDate(f),degreeDate(d),idCard(id) {}
 
-std::string Formation::getGradeName()
-{
+std::string Formation::getGrade(){
+    return grade;
+}
+
+std::string Formation::getGradeName() {
     return gradeName;
-}
+    }
 
-std::string Formation::getInstitution()
-{
+std::string Formation::getInstitution() {
     return institution;
-}
+    }
 
-Date Formation::getBeginDate()
-{
+Date Formation::getBeginDate() {
     return beginDate;
-}
+    }
 
-Date Formation::getFinishDate()
-{
+Date Formation::getFinishDate() {
     return finishDate;
-}
+    }
 
-Date Formation::getDegreeDate()
-{
+Date Formation::getDegreeDate() {
     return degreeDate;
-}
+    }
 
-std::string Formation::getIdCard()
-{
+std::string Formation::getIdCard() {
     return idCard;
-}
+    }
 
-void Formation::setGradeName(const std::string& n)
-{
+void Formation::setGrade(const std::string& g) {
+    grade = g;
+    }
+
+void Formation::setGradeName(const std::string& n) {
     gradeName = n;
-}
+    }
 
-void Formation::setInstitution(const std::string& i)
-{
+void Formation::setInstitution(const std::string& i) {
     institution = i;
-}
+    }
 
-void Formation::setBeginDate(const Date& b)
-{
+void Formation::setBeginDate(const Date& b) {
     beginDate = b;
-}
+    }
 
-void Formation::setFinishDate(const Date& f)
-{
+void Formation::setFinishDate(const Date& f) {
     finishDate = f;
-}
+    }
 
-void Formation::setDegreeDate(const Date& d)
-{
+void Formation::setDegreeDate(const Date& d) {
     degreeDate = d;
-}
+    }
 
-void Formation::setIdCard(const std::string& id)
-{
+void Formation::setIdCard(const std::string& id) {
     idCard = id;
-}
+    }
 
 /*void Formation::toString()
 {
-
 }*/
